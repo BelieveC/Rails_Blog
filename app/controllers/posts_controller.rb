e5301d926a785a66640f4f71ba0a092dcee03442
Chaitanya Yadav <cayadav2009@gmail.com>
2016-03-20 21:44:19 +0530

    Added Post Functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,30 @@
 class PostsController < ApplicationController
 
 	def index
+		@posts = Post.all.order("created_at DESC")
 	end
+
+	def new
+		@post = Post.new
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+
+	def create
+		@post = Post.new(posts_params)
+		if @post.save
+			redirect_to(@post)
+		else
+			render('new')
+		end
+	end
+
+
+	private
+		def posts_params
+			params.require(:post).permit(:title,:body)
+		end
 end
