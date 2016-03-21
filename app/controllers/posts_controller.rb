dd68aa076c428bc3beba85836926e896ed5c6594
Chaitanya Yadav <cayadav2009@gmail.com>
2016-03-21 21:01:32 +0530

    Edit and Delete functionality added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,13 @@
 class PostsController < ApplicationController
 
+	before_action :get_post, except: [:index, :new,:create]
+
 	def index
 		@posts = Post.all.order("created_at DESC")
+	end
+
+	def edit
+		
 	end
 
 	def new
@@ -9,7 +15,7 @@ class PostsController < ApplicationController
 	end
 
 	def show
-		@post = Post.find(params[:id])
+		
 	end
 
 
@@ -22,9 +28,27 @@ class PostsController < ApplicationController
 		end
 	end
 
+	def update
+		if @post.update(posts_params)
+			flash[:notice] = "Yay! Post Updated Successfully."
+			redirect_to @post
+		else
+			render('edit')
+		end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to root_path, notice: "Successfully Deleted the Post!"
+	end
+
 
 	private
 		def posts_params
 			params.require(:post).permit(:title,:body)
+		end
+
+		def get_post
+			@post = Post.find(params[:id])
 		end
 end
